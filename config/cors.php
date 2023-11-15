8bbdb9cb4bf6125f7604104a75dd3758256f4ee8
Santiago Inostroza <santiagoinostroza2@gmail.com>
2023-11-15 18:03:46 -0300

    Se dan permisos globales al api

--- config/cors.php
+++ config/cors.php
@@ -19,8 +19,8 @@ return [
 
     'allowed_methods' => ['*'],
 
-    // 'allowed_origins' => ['*'],
-    'allowed_origins' => [' https://santiagoinostroza.github.io'],
+    'allowed_origins' => ['*'],
+    // 'allowed_origins' => [' https://santiagoinostroza.github.io'],
 
     'allowed_origins_patterns' => [],
 
