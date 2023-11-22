b0d0065d52ef242f0247f851fc196b569e4e13c0
Santiago Inostroza <santiagoinostroza2@gmail.com>
2023-11-22 16:32:29 -0300

    se agrega localhost a cors

--- config/cors.php
+++ config/cors.php
@@ -20,7 +20,7 @@ return [
     'allowed_methods' => ['*'],
 
     // 'allowed_origins' => ['*'],
-    'allowed_origins' => [' https://santiagoinostroza.github.io'],
+    'allowed_origins' => [' https://santiagoinostroza.github.io', 'http://localhost:5173'],
 
     'allowed_origins_patterns' => [],
 
